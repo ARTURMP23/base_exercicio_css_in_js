176c2f3430e096b2fd2f3eb121ff584c1ca3ddc7
Artur Peixoto <arturrtutu@gmail.com>
2025-01-06 15:14:44 -0300

    adicionando styled-components

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,4 +1,7 @@
-.form {
+import styled from 'styled-components'
+
+export const Form = styled.form`
+{
   height: 360px;
   width: 100%;
   background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
@@ -6,9 +9,8 @@
   position: relative;
   display: flex;
   align-items: center;
-}
 
-.form::before {
+  &:before {
   position: absolute;
   top: 0;
   left: 0;
@@ -17,25 +19,31 @@
   background-color: var(--cor-principal);
   content: '';
   opacity: 0.7;
-}
+  }
 
-.form div {
-  position: relative;
-  color: #eee;
-}
+  @media (max-width: 768px) {
 
-.heroTitle {
-  font-family: Gloock, serif;
-  font-size: 48px;
-}
-
-@media (max-width: 768px) {
-  .form {
     height: auto;
     padding: 24px 0;
+
+}
+
+`
+
+export const Div = styled.div`
+   {
+    position: relative;
+    color: #eee;
+  }
+`
+
+export const Titulo = styled.h2`
+   {
+    font-family: Gloock, serif;
+    font-size: 48px;
   }
 
-  .heroTitle {
+  @media (max-width: 768px) {
     font-size: 32px;
   }
-}
+`
